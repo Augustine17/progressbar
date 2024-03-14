a1250c1ab89dde18df63214f79111777d0f2e202
Augustine71 <amenezes927@gmail.com>
2024-03-14 22:20:58 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+  const[status,setStatus]=useState("Loading...");
+  const[progress,setProgress]=useState(0);
+
+  useEffect(()=>{
+    if(progress<100){
+      setTimeout(()=>{
+        setProgress(prev=>prev+1);
+      },500)
+    }else{
+      setStatus("Completed!!!")
+    }
+    
+  },[progress])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Progress Bar</h1>
+
+      <div className='bar'>
+        <div className='progress' style={{height:"100%", width:`${progress}%`}}>
+        </div>
+        <span style={{color: progress >= 60 ? '#fff' : '#000'}}>{progress}%</span>
+      </div>
+
+      <span>{status}</span>
     </div>
   );
 }
